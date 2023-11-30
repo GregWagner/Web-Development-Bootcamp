3b6c6ca9f3254bc8267cc6c2fa61b466c9c069b1
gwagner <theGreg101@gmail.com>
2023-11-30 16:30:35 -0600

    Added Yelpcamp Reviews

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -16,11 +16,17 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
+
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://random.imagecdn.app/500/150',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora est temporibus quo natus nisi! Blanditiis magni quis debitis voluptas, sunt natus sequi aliquam eius voluptatem odit ab voluptate sint dignissimos.',
+      price,
     });
     await camp.save();
   }
